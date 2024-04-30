703225dc07efc828e583fd04579bad0ec417d74a
Ondrej Odehnal <OndrejOdehnal42@gmnail.com>
2024-04-30 14:45:32 +0300

    FIX: paths

--- src/diarization/diarization.py
+++ src/diarization/diarization.py
@@ -10,6 +10,7 @@
 # =============================================================================
 import torch
 import argparse
+import yaml
 import os
 import pandas as pd
 
@@ -39,7 +40,20 @@ if __name__ == "__main__":
     os.makedirs(args.out_dir, exist_ok=True)
 
     script_path = os.path.dirname(__file__)
-    pipeline = Pipeline.from_pretrained(f"{script_path}/config.yaml")
+
+    # Load the existing configuration
+    with open(os.path.join(script_path, "config.yaml"), 'r') as file:
+            config = yaml.safe_load(file)
+
+    # Update the segmentation path to be relative to the script folder
+    config['pipeline']['params']['segmentation'] = os.path.join(script_path, config['pipeline']['params']['segmentation'])
+
+    tmp_config = os.path.join(script_path, '.config.yaml.tmp')
+    with open(tmp_config, 'w') as file:
+        yaml.safe_dump(config, file)
+
+    pipeline = Pipeline.from_pretrained(tmp_config)
+
     pipeline.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
     for file in tqdm(args.files):
@@ -60,7 +74,7 @@ if __name__ == "__main__":
             'Speaker': speakers
         })
 
-        df.to_csv(os.path.join('labels', f"{os.path.basename(file).split('.')[0]}.csv"), index=False)
+        df.to_csv(os.path.join(args.out_dir, f"{os.path.basename(file).split('.')[0]}.csv"), index=False)
 
         df2 = df[['Speaker']].copy()
         df2['Duration'] = df['End [s]'] - df['Start [s]']
@@ -73,4 +87,4 @@ if __name__ == "__main__":
             df_filtered = pd.DataFrame(columns=df.columns)
         else:
             df_filtered = df.drop(df[df['Speaker'] != min_speaker_id].index)
-        df_filtered.to_csv(os.path.join('labels', f"{os.path.basename(file).split('.')[0]}_unwanted.csv"), index=False)
+        df_filtered.to_csv(os.path.join(args.out_dir, f"{os.path.basename(file).split('.')[0]}_unwanted.csv"), index=False)
